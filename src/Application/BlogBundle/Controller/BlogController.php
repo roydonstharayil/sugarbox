a88ab5af5dfa858d06537b6710be7d3c712111f2
Roydon <roydonstharayil@gmail.com>
2010-08-24 19:29:50 +0530

    delete some methods

--- src/Application/BlogBundle/Controller/BlogController.php
+++ src/Application/BlogBundle/Controller/BlogController.php
@@ -3,13 +3,10 @@
 namespace Application\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller as Controller;
-//use Symfony\Component\HttpFoundation\Request;
-//use Symfony\Component\HttpKernel\HttpKernel;
 use Symfony\Component\Form\Form;
 use Application\BlogBundle\Form\BlogForm;
 use Application\BlogBundle\Entity\Blog;
 use Symfony\Component\EventDispatcher\Event;
-//use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class BlogController extends Controller
 {
@@ -17,58 +14,25 @@ class BlogController extends Controller
     
     public function indexAction()
     {
-        $request = $_REQUEST;
-
+       
         return $this->render('BlogBundle:Blog:index');
 
-        // render a Twig template instead
-        // return $this->render('HelloBundle:Hello:index:twig', array('name' => $name));
+        
     }
     public function newAction()
     {
         $blog = new Blog();
-//        $request = $this->getRequest();
+
         $form = new BlogForm('blog', $blog, $this->container->getValidatorService());
-//        TODO
-        if('POST' === $this->getRequest()) {
-            $form->bind($this->get('blog'));
-//        if ($this->getRequest()->getMethod() == 'POST') {
-//            $form->bind('blog');
-            if($form->isValid()) {
-                $this->getEntityManager()->persist($blog);
-                $this->getEntityManager()->flush();
-//
-////                $this->container->getEventDispatcherService()->notify(new Event($project, 'miam.project.create'));
-////
-////                $this->container->getSessionService()->setFlash('project_create', array('project' => $project));
-////                return $this->redirect($this->generateUrl('projects'));
-//            }
-//        }
-        
-
-
-    }
-        }
+        //TODO
+       
         return $this->render('BlogBundle:Blog:new', array(
             'form' => $form
         ));
         
     }
     protected function getEntityManager()
-    {
-        return $this->container->getDoctrine_ORM_EntityManagerService();
-
-    }
-    protected function getRequest()
-    {
-        return 'POST';
-    }
-        protected function get($bloarr)
-        {
-           $blogarray = array('title' => '', 'description' => '');
-           return $blogarray;
-        }
-   
+    
     
 }
     
